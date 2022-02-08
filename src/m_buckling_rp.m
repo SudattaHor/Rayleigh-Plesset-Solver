8145ab9351ae7b848e48801cdca092e8becd0d87
SudattaHor <sudatta_hor@brown.edu>
2022-02-07 22:02:12 -0500

    entered equation for f_1 and f_2

--- src/m_buckling_rp.m
+++ src/m_buckling_rp.m
@@ -7,9 +7,22 @@ function drdt = m_buckling_rp(t, r, constants)
     % OUTPUTS:
     %   drdt - a two-dimensional vector [f_1; f_2] where
     %          f_1 = r' and f_2 = r''
-    %
-    % TODO - IMPLEMENT
-    %
+    % UNPACK
+    R = r(1);
+    Rprime = r(2);
+    % TERMS for f_2
+    b1 = P0 + (2 * f_sigma(R0, constants) / R0);
+    b2 = (R / R0)^(-3*kappa);
+    b3 = 1 - (3 * kappa * Rprime / c);
+    a0 = b1 * b2 * b3 / (rho_L * R);
+    a1 = -P0 / (rho_L * R);
+    a2 = -2 * f_sigma(R, constants) / (rho_L * R^2);
+    a3 = -4 * mu * Rprime / (rho_L * R^2);
+    a4 = -4 * kappa_s * Rprime / (rho_L * R^3);
+    a5 = -P_ac(t);
+    % COMPONENTS
+    f_1 = Rprime;
+    f_2 = a0 + a1 + a2 + a3 + a4 + a5;
     % SOLUTION
-    drdt = [t*0, r*0];
+    drdt = [f_1; f_2];
 end
