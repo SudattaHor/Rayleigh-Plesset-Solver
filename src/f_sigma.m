5758be6e3e17ba1c94ec80afd11c2614c472b27c
SudattaHor <sudatta_hor@brown.edu>
2022-02-12 22:03:24 -0500

    updated piecewise to include missing condition for R_ruptured

--- src/f_sigma.m
+++ src/f_sigma.m
@@ -1,15 +1,18 @@
 function S = f_sigma(R, constants)
     % solves for sigma(r)
     % UNPACK
-    R_buckling = constants(8);
-    R_break_up = constants(9);
+    R_buckling = constants(7);
+    R_break_up = constants(8);
+    R_ruptured = constants(9);
     kai = constants(10);
     S_water = constants(11);
     % EVALUATE
     if (R <= R_buckling)
         S = 0;
-    elseif (R < R_break_up)
+    elseif (R <= R_break_up)
         S = kai * ((R/R_buckling)^2 - 1);
+    elseif (R <= R_ruptured)
+        S = kai * ((R/R_ruptured)^2 - 1);
     else
         S = S_water;
     end
