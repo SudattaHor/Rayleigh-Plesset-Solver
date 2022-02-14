062aa8dd5a8cf035e9c9e4e90cfbf403376a85cf
SudattaHor <sudatta_hor@brown.edu>
2022-02-13 21:51:35 -0500

    combined pressure terms into f_p

--- src/f_p_ac.m
+++ src/f_p_ac.m
@@ -1,8 +1,5 @@
 function pressure = f_p_ac(t, constants)
-    % Calculates P_ac(t)
-    % UNPACK
-    P0 = constants(4);
-    % EVALUATE
-    pressure = 10*P0 + t*0;
+    % Computes driving acoustical pressure
+    pressure = 0;
 end
 
--- src/m_buckling_rp.m
+++ src/m_buckling_rp.m
@@ -22,15 +22,14 @@ function drdt = m_buckling_rp(t, r, constants)
     b2 = (R / R0)^(-3*kappa);
     b3 = 1 - (3 * kappa * Rprime / c);
     a0 = b1 * b2 * b3 / (rho_L * R);
-    a1 = -P0 / (rho_L * R);
-    a2 = -2 * f_sigma(R, constants) / (rho_L * R^2);
-    a3 = -4 * mu * Rprime / (rho_L * R^2);
-    a4 = -4 * kappa_s * Rprime / (rho_L * R^3);
-    a5 = -f_p_ac(t, constants) / (rho_L * R);
-    a6 = -3 * Rprime^2 / (2*R);
+    a1 = -2 * f_sigma(R, constants) / (rho_L * R^2);
+    a2 = -4 * mu * Rprime / (rho_L * R^2);
+    a3 = -4 * kappa_s * Rprime / (rho_L * R^3);
+    a4 = -f_p(t, constants) / (rho_L * R);
+    a5 = -3 * Rprime^2 / (2*R);
     % COMPONENTS
     f_1 = Rprime;
-    f_2 = a0 + a1 + a2 + a3 + a4 + a5 + a6;
+    f_2 = a0 + a1 + a2 + a3 + a4 + a5;
     % SOLUTION
     drdt = [f_1; f_2];
 end
